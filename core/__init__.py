a3a9bf8f7f3269a3a0c753afe42ee0ac7e659d06
wellcomeai <19wellai96@gmail.com>
2025-09-14 11:33:35 +0300

    Create __init__.py

---
+++ core/__init__.py
@@ -0,0 +1,6 @@
+"""
+Базовые компоненты системы
+"""
+from .exceptions import BotException, ConfigurationError, ConnectionError, StrategyError
+
+__all__ = ['BotException', 'ConfigurationError', 'ConnectionError', 'StrategyError']
